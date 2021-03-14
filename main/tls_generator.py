35f8dc41a2f16601d02f5ef0b0f7d7d85dd80e0e
Farhan Nugroho <farhannyusuf@gmail.com>
2021-03-14 14:03:59 -0700

    Update tsl_generator to remove unused import and format style

--- main/tls_generator.py
+++ main/tls_generator.py
@@ -1,10 +1,10 @@
-import socket
 import random
 from OpenSSL import crypto
 from datetime import datetime
 
 hostname = "exoduspi.com"
 currentDate = str(datetime.now().date())
+
 
 def gen_self_signed_cert():
     pkey = crypto.PKey()
@@ -15,20 +15,29 @@ def gen_self_signed_cert():
     subject.commonName = hostname
     x509.set_issuer(subject)
     x509.gmtime_adj_notBefore(0)
-    x509.gmtime_adj_notAfter(5*365*24*60*60)
+    x509.gmtime_adj_notAfter(5 * 365 * 24 * 60 * 60)
     x509.set_pubkey(pkey)
     x509.set_serial_number(random.randrange(100000))
     x509.set_version(2)
-    x509.add_extensions([
-        crypto.X509Extension(b'subjectAltName', False,
-            ','.join([
-                f'DNS:{hostname}',
-                f'DNS:*.{hostname}',
-                'DNS:localhost',
-                'DNS:*.localhost']).encode()),
-        crypto.X509Extension(b"basicConstraints", True, b"CA:false")])
+    x509.add_extensions(
+        [
+            crypto.X509Extension(
+                b"subjectAltName",
+                False,
+                ",".join(
+                    [
+                        f"DNS:{hostname}",
+                        f"DNS:*.{hostname}",
+                        "DNS:localhost",
+                        "DNS:*.localhost",
+                    ]
+                ).encode(),
+            ),
+            crypto.X509Extension(b"basicConstraints", True, b"CA:false"),
+        ]
+    )
 
-    x509.sign(pkey, 'SHA256')
+    x509.sign(pkey, "SHA256")
 
     crtpath = f"{hostname}-{currentDate}.crt"
     keypath = f"{hostname}-{currentDate}.key"
@@ -44,5 +53,6 @@ def gen_self_signed_cert():
     print("Success")
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     gen_self_signed_cert()
